ddfe4b858ae06fc244d37363f9cb17b82c9273d4
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-11-01 18:32:44 -0500

    update

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -14,11 +14,12 @@ export class ProductService {
   
   products$ = this.http.get<Product[]>(this.productsUrl)
   .pipe(
-    map(products  => products.map( product => ({ 
-      ...product,
-      price: product.price ? product.price * 1.5 : 0,
-      searchKey: [product.productName]
-    } as Product))),
+    map(products  => 
+      products.map(product  => ({
+        ...product,
+        price: product.price ? product.price * 1.5 :0,
+        searchKey: [product.productName]
+      } as Product))),
     tap(data => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)
   );
